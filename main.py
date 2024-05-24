e865ef022531a4b5085eb575dd7745b3b726b113
YasmineZaatour <117782662+YasmineZaatour@users.noreply.github.com>
2024-05-24 18:50:35 +0100

    analyze the home page

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from pprint import pprint
 
 url="https://books.toscrape.com/"
 response=requests.get(url)
 
 soup=BeautifulSoup(response.text,'html.parser')
-print(soup.prettify())
+aside=soup.find('div',class_='side_categories')
+categories_div=aside.find('ul').find('li').find('ul')
+categories=[child.text.strip() for child in categories_div.children if child.name]
 
+images=soup.find('section').find_all('img')
+images=[img.get('src') for img in images]
+pprint(images)
 
  
